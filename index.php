7df73e6caa1ddbe3c4a240ed79d5e839242891d9
Ashiqur Rahman <arshohag8@gmail.com>
2017-01-05 13:21:00 +0600

    Popup v_1.0.0
    Popup For any websites

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+<!-- Basic Page Needs
+================================================== -->
+	<meta charset="utf-8">
+	<title>Popup | ARshohag</title>
+	<meta name="description" content="POPUP-ARShohag">
+	<meta name="author" content="Ashiqur Rahman">
+    <meta name="web_author" content="Ashiqur Rahman, Web App Engineer, arshohag@techcarebd.com, arshohag.activegroup-bd.com" />
+
+<!-- Mobile Specific Metas  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- New Year popup library CSS
+================================================== -->
+
+    <link rel="stylesheet" href="popup/main.css">		
+	
+
+<!-- New Year popup Script
+================================================== -->    
+	<script type="text/javascript">// <![CDATA[
+        function preloader(){
+            document.getElementById("loading").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }//preloader
+        window.onload = preloader;
+// ]]></script>
+
+</head>
+
+<body>
+<!-- New Year popup HTML
+================================================== -->       
+    <div id="boxes">
+        <div style="top: 199.5px; left: 551.5px; display: none;" id="dialog" class="window"> Happy New Year <br> <br>
+            <div id="lorem">
+                <img src="popup/2017.gif" width="450"/>
+            </div>
+
+        </div>
+        <div style="width: 1478px; font-size: 32pt; color:white; height: 602px; display: none; opacity: 0.8;" id="mask">
+        </div>
+    </div>
+    
+<!-- New Year popup library Javascript
+================================================== -->
+
+<script src="popup/main.js"></script>
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
